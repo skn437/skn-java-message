ecf2c855ed97fd1c087ed9814a07e58c71c5dfd9
skn437 <skn437physx@gmail.com>
2024-03-15 02:18:06 +0600

    Updated JavaDocs A Bit

--- src/main/java/best/skn/utils/message/package-info.java
+++ src/main/java/best/skn/utils/message/package-info.java
@@ -2,9 +2,9 @@
  * Utility Message Package
  *
  * @author SKN
- * @version 1.0.0
+ * @version 1.0.1
  * @since 2024-03-12
  * @use.case Java, Java Spring Boot
- * @dedicated.to Trina, Logno and Atoshi
+ * @dedicated.to Trina, Logno, Atoshi and My Parents
  */
 package best.skn.utils.message;
